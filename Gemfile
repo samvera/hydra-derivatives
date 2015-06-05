9bce6e0c8dafb454582efd48ab9d6a485a8bb088
Matt Zumwalt <matt@curationexperts.com>
2015-06-05 10:14:42 -0500

    Test for compatibility with hydra-pcdm and hydra-works

--- Gemfile
+++ Gemfile
@@ -5,4 +5,11 @@ gemspec
 
 group :development, :test do
   gem 'byebug' unless ENV['TRAVIS']
+
+  # Not-Yet-Released Development dependencies
+  gem 'hydra-works', github: 'projecthydra-labs/hydra-works', ref: '013d817'
+  gem 'hydra-pcdm', github: 'projecthydra-labs/hydra-pcdm', ref: '30a9643'
+  gem 'active-fedora', github: 'projecthydra/active_fedora', ref:'57ac754'
+  gem 'activefedora-aggregation', github: 'projecthydra-labs/activefedora-aggregation', ref: 'eef02b0'
+
 end
---
+++ spec/units/processor_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'hydra/works'
+
+describe Hydra::Derivatives::Processor do
+
+  before(:all) do
+    class IndirectContainerObject < ActiveFedora::Base
+      contains "content"
+      contains "thumbnail"
+    end
+
+    # This uses directly_contains (inherited from Hydra::PCDM::ObjectBehavior)
+    class DirectContainerObject < Hydra::Works::GenericFile::Base
+    end
+    # If you manually built DirectContainerObject, it would look like this:
+    # class DirectContainerObject < ActiveFedora::Base
+    #
+    #   directly_contains :files, has_member_relation: RDFVocabularies::PCDMTerms.hasFile,
+    #     class_name: "Hydra::PCDM::File"
+    #
+    #   def original_file
+    #     file_of_type(::RDF::URI("http://pcdm.org/OriginalFile"))
+    #   end
+    #
+    #   def thumbnail
+    #     file_of_type(::RDF::URI("http://pcdm.org/ThumbnailImage"))
+    #   end
+    # end
+  end
+
+  let(:object)        { IndirectContainerObject.new  }
+  let(:source_name)   { 'content' }
+  let(:directives)    { { thumb: "100x100>" } }
+
+  subject { Hydra::Derivatives::Processor.new(object, source_name, directives)}
+
+  describe "source_file" do
+    it "retrieves the specified source" do
+      expect(subject.source_file).to eq(object.content)
+    end
+  end
+
+  describe "output_file" do
+    it "retrieves the correct output file" do
+      expect(subject.output_file('thumbnail')).to eq(object.thumbnail)
+    end
+  end
+
+  describe "when files are directly contained by object (like files in a PCDM::Object)" do
+    let(:object)        { DirectContainerObject.new }
+    let(:source_name)   { 'original_file' }
+    before do
+      object.save
+    end
+    it "is able to find source_file and output_file" do
+      expect(subject.source_file).to eq(object.original_file)
+      expect(subject.output_file).to eq(object.thumbnail)
+    end
+  end
+end
