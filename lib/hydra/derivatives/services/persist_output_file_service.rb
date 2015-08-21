83ee9a243fafc8af803207f36ef860beb039a46b
Justin Coyne <jcoyne@justincoyne.com>
2015-08-21 13:22:55 -0500

    Update documentation to show IoDecorator

--- lib/hydra/derivatives/services/persist_output_file_service.rb
+++ lib/hydra/derivatives/services/persist_output_file_service.rb
@@ -4,7 +4,7 @@ module Hydra::Derivatives
     # Persists the file within the object at destination_name.  Uses basic containment.
     # If you want to use direct containment (ie. with PCDM) you must use a different service (ie. Hydra::Works::AddFileToGenericFile Service)
     # @param [Object] object the source file is attached to
-    # @param [File] filestream to be added, should respond to :mime_type, :original_name
+    # @param [Hydra::Derivatives::IoDecorator] filestream to be added
     # @param [String] destination_name is the fedora path at which the child resource will be found or created beneath the object.
 
     def self.call(object, file, destination_path)
