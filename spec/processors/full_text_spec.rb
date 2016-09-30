010135cbd7d4c8740a1de71e78ac6615042074b3
Adam Wead <amsterdamos@gmail.com>
2016-09-30 16:03:59 -0400

    Enabling full text spec test

--- spec/processors/full_text_spec.rb
+++ spec/processors/full_text_spec.rb
@@ -1,20 +1,11 @@
 require 'spec_helper'
 
 describe Hydra::Derivatives::Processors::FullText do
-  let(:file_path) { File.join(fixture_path, 'test.docx') }
-  let(:directives) { { format: 'txt', url: 'http://localhost:8983/fedora/rest/dev/1234/ogg' } }
-  let(:processor) { described_class.new(file_path, directives) }
+  let(:file_path)  { File.join(fixture_path, 'test.docx') }
+  let(:directives) { { format: 'txt', url: RDF::URI('http://localhost:8983/fedora/rest/dev/1234/ogg') } }
+  let(:processor)  { described_class.new(file_path, directives) }
 
-  describe "process" do
-    subject { processor.process }
-
-    context "when it is successful" do
-      before do
-        allow_any_instance_of(described_class).to receive(:fetch).and_return('{"":"one two three"}')
-      end
-      it { is_expected.to be true }
-    end
-
+  describe "#process" do
     it 'extracts fulltext and stores the results' do
       expect(processor.output_file_service).to receive(:call).with(/Project Charter for E-Content Delivery Platform Review/, directives)
       processor.process
@@ -22,14 +13,14 @@ describe Hydra::Derivatives::Processors::FullText do
   end
 
   describe "fetch" do
-    subject { processor.send(:fetch) }
-    let(:request) { double }
+    let(:request)       { double }
     let(:response_body) { 'returned by Solr' }
-    let(:resp) { double(code: '200', body: response_body) }
-    let(:uri) { URI('http://example.com:99/solr/update') }
+    let(:uri)           { URI('http://example.com:99/solr/update') }
+
+    subject { processor.send(:fetch) }
 
     before do
-      allow(processor).to receive(:uri).and_return(URI('http://example.com:99/solr/update'))
+      allow(processor).to receive(:uri).and_return(uri)
       allow(Net::HTTP).to receive(:new).with('example.com', 99).and_return(request)
     end
 
@@ -45,7 +36,7 @@ describe Hydra::Derivatives::Processors::FullText do
       let(:resp) { double(code: '500', body: response_body) }
       it "raises an error" do
         expect(request).to receive(:post).with('http://example.com:99/solr/update', String, "Content-Type" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "Content-Length" => "24244").and_return(resp)
-        expect { subject }.to raise_error(RuntimeError, "Solr Extract service was unsuccessful. 'http:\/\/example\.com:99\/solr\/update' returned code 500 for .*spec\/fixtures\/test.docx\nreturned by Solr")
+        expect { subject }.to raise_error(RuntimeError, %r{^Solr Extract service was unsuccessful. 'http://example\.com:99/solr/update' returned code 500})
       end
     end
   end
