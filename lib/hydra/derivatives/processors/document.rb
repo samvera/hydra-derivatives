ade91910414a1821c110d97a441941be0c1ee6ca
Adam Wead <amsterdamos@gmail.com>
2016-10-12 11:07:46 -0400

    Convert documents to pdf and jpg, fixes #109
    This improves the function of Hydra::Derivatives::Processors::Document and refactors its tests.
    
    * correctly converts documents to pdf and other non-image formats
    * jpg conversion is delegated to Hydra::Derivatives::Processors::Image so files are correctly resized
    * adds additional units tests; refactors existing tests

--- lib/hydra/derivatives/processors/document.rb
+++ lib/hydra/derivatives/processors/document.rb
@@ -2,27 +2,42 @@ module Hydra::Derivatives::Processors
   class Document < Processor
     include ShellBasedProcessor
 
-    def self.encode(path, _options, output_file)
-      format = File.extname(output_file).sub('.', '')
-      outdir = File.dirname(output_file)
+    def self.encode(path, format, outdir)
       execute "#{Hydra::Derivatives.libreoffice_path} --invisible --headless --convert-to #{format} --outdir #{outdir} #{path}"
     end
 
-    def encode_file(file_suffix, options = {})
-      new_output = ''
-      if file_suffix == 'jpg'
-        temp_file = File.join(Hydra::Derivatives.temp_file_base, [directives.fetch(:label).to_s, 'pdf'].join('.'))
-        new_output = File.join(Hydra::Derivatives.temp_file_base, [File.basename(temp_file).sub(File.extname(temp_file), ''), file_suffix].join('.'))
-        self.class.encode(source_path, options, temp_file)
-        self.class.encode(temp_file, options, output_file(file_suffix))
-        File.unlink(temp_file)
-      else
-        self.class.encode(source_path, options, output_file(file_suffix))
-        new_output = File.join(Hydra::Derivatives.temp_file_base, [directives.fetch(:label).to_s, file_suffix].join('.'))
-      end
-      out_file = File.open(new_output, "rb")
-      output_file_service.call(out_file, directives)
-      File.unlink(out_file)
+    # Converts the document to the format specified in the directives hash.
+    # TODO: file_suffix and options are passed from ShellBasedProcessor.process but are not needed.
+    #       A refactor could simplify this.
+    def encode_file(_file_suffix, _options = {})
+      convert_to_format
+    ensure
+      FileUtils.rm_f(converted_file)
     end
+
+    private
+
+      # For jpeg files, a pdf is created from the original source and then passed to the Image processor class
+      # so we can get a better conversion with resizing options. Otherwise, the ::encode method is used.
+      def convert_to_format
+        if directives.fetch(:format) == "jpg"
+          Hydra::Derivatives::Processors::Image.new(converted_file, directives).process
+        else
+          output_file_service.call(File.read(converted_file), directives)
+        end
+      end
+
+      def converted_file
+        @converted_file ||= if directives.fetch(:format) == "jpg"
+                              convert_to("pdf")
+                            else
+                              convert_to(directives.fetch(:format))
+                            end
+      end
+
+      def convert_to(format)
+        self.class.encode(source_path, format, Hydra::Derivatives.temp_file_base)
+        File.join(Hydra::Derivatives.temp_file_base, [File.basename(source_path, ".*"), format].join('.'))
+      end
   end
 end
