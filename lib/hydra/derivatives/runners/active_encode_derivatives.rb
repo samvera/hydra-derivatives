9bc5736ff22b7d1d1d147defc056c9c6ac8a1d76
val99erie <val99erie@gmail.com>
2017-04-07 11:33:10 -0500

    Wait for encoding to finish.  Part of story avalonmediasystem/avalon#1785

--- lib/hydra/derivatives/runners/active_encode_derivatives.rb
+++ lib/hydra/derivatives/runners/active_encode_derivatives.rb
@@ -1,20 +1,19 @@
 module Hydra::Derivatives
   class ActiveEncodeDerivatives < Runner
+    # TODO: object_or_filename - I'm currently passing a String
+    # filename during my testing, but we'll probably need to
+    # change this so that we can pass in the FileSet or File
+    # object that the source file is attached to.
+    #
     # @param [String, ActiveFedora::Base] object_or_filename path to the source file, or an object
     # @param [Hash] options options to pass to the encoder
-    # @options options [Array] :outputs a list of desired outputs, each entry is a hash that has :label (optional), :format and :url
+    # @options options [Array] :outputs a list of desired outputs
     def self.create(object_or_filename, options)
-      source_file(object_or_filename, options) do |f|
-        instructions = transform_directives(options)
-        processor_class.new(f,
-                            instructions.merge(source_file_service: source_file_service),
-                            output_file_service: output_file_service).process
+      file_name = object_or_filename
+      transform_directives(options.delete(:outputs)).each do |instructions|
+        processor = processor_class.new(file_name, instructions, output_file_service: output_file_service)
+        processor.process
       end
-    end
-
-    # Use the source service configured for this class or default to the uri source service
-    def self.source_file_service
-      @output_file_service || UriSourceFileService
     end
 
     # Use the output service configured for this class or default to the null output service
