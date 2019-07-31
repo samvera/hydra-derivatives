81260cf2101531b2a95442165b316083c722e4ac
Trey Pendragon <tpendragon@princeton.edu>
2019-07-31 11:55:30 -0700

    Cover runner.rb

---
+++ spec/runners/runner_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe Hydra::Derivatives::Runner do
+  describe ".processor_class" do
+    it "raises an error if it's not overridden" do
+      expect { described_class.processor_class }.to raise_error "Overide the processor_class method in a sub class"
+    end
+  end
+end
