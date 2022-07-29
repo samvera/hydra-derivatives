6ae20f803cabb20f6128cf3dd5e76d335aa7892a
Chris Colvard <chris.colvard@gmail.com>
2022-07-29 12:38:41 -0400

    Appease rubocop

--- lib/hydra/derivatives/processors/video/processor.rb
+++ lib/hydra/derivatives/processors/video/processor.rb
@@ -7,21 +7,21 @@ module Hydra::Derivatives::Processors
       self.config = Config.new
 
       protected
-      def options_for(format)
-        output_options = "-s #{@directives[:size].nil? ? config.size_attributes : @directives[:size]} "
-        output_options += "#{codecs(format)}"
-        if format == "jpg"
-          input_options = " -itsoffset -2"
-          output_options += " -vframes 1 -an -f rawvideo"
-        else
-          input_options = @directives[:input_options].nil? ? "" : @directives[:input_options]
-          output_options += " #{@directives[:video].nil? ? config.video_attributes : @directives[:video]}"
-          output_options += " #{@directives[:audio].nil? ? config.audio_attributes : @directives[:audio]}"
+
+        def options_for(format)
+          output_options = "-s #{@directives[:size].nil? ? config.size_attributes : @directives[:size]} "
+          output_options += codecs(format).to_s
+          if format == "jpg"
+            input_options = " -itsoffset -2"
+            output_options += " -vframes 1 -an -f rawvideo"
+          else
+            input_options = @directives[:input_options].nil? ? "" : @directives[:input_options]
+            output_options += " #{@directives[:video].nil? ? config.video_attributes : @directives[:video]}"
+            output_options += " #{@directives[:audio].nil? ? config.audio_attributes : @directives[:audio]}"
+          end
+
+          { Ffmpeg::OUTPUT_OPTIONS => output_options, Ffmpeg::INPUT_OPTIONS => input_options }
         end
-
-        { Ffmpeg::OUTPUT_OPTIONS => output_options, Ffmpeg::INPUT_OPTIONS => input_options }
-      end
-
 
         def codecs(format)
           case format
