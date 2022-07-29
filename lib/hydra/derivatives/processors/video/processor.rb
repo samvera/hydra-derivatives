3ffc9f15bfa57c51cccc54312cd5674180df5e58
Chris Colvard <chris.colvard@gmail.com>
2022-07-29 12:34:22 -0400

    Allow total customization of video and audio options

--- lib/hydra/derivatives/processors/video/processor.rb
+++ lib/hydra/derivatives/processors/video/processor.rb
@@ -8,14 +8,15 @@ module Hydra::Derivatives::Processors
 
       protected
       def options_for(format)
-        input_options = ""
         output_options = "-s #{@directives[:size].nil? ? config.size_attributes : @directives[:size]} "
         output_options += "#{codecs(format)}"
         if format == "jpg"
-          input_options += " -itsoffset -2"
+          input_options = " -itsoffset -2"
           output_options += " -vframes 1 -an -f rawvideo"
         else
-          output_options += " #{@directives[:bitrate].nil? ? config.video_attributes : "-g 30 -b:v "+@directives[:bitrate]} #{config.audio_attributes}"
+          input_options = @directives[:input_options].nil? ? "" : @directives[:input_options]
+          output_options += " #{@directives[:video].nil? ? config.video_attributes : @directives[:video]}"
+          output_options += " #{@directives[:audio].nil? ? config.audio_attributes : @directives[:audio]}"
         end
 
         { Ffmpeg::OUTPUT_OPTIONS => output_options, Ffmpeg::INPUT_OPTIONS => input_options }
